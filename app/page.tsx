f2afa09299711473a2b214a93f304115bb265411
narugit <narusens@gmail.com>
2023-08-15 10:37:04 +0900

    fix: change directory structure of app/
    Move src/app/ to app/
    
    JIRA: LT-26

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
+          <code className={styles.code}>app/page.tsx</code>
         </p>
         <div>
           <a
