b91aeab627e15862278a73f97fa7267ed401c04b
hiyoko3 <16721102+hiyoko3@users.noreply.github.com>
2023-11-20 22:51:11 +0900

    refactor: rename variable

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,16 +1,11 @@
 import { defineConfig } from 'cypress'
 
-let environment: string | undefined = process.env.RUN_FQDN
-
-if (environment === undefined) {
-  environment = 'localhost'
-}
+const HOST_FQDN: string = process.env.HOST_FQDN || 'localhost'
 
 // Configuration documentation: https://docs.cypress.io/guides/references/configuration
 export default defineConfig({
   e2e: {
-    // If you run on Docker, you need to use `web:3000` instead of `localhost:3000`.
-    baseUrl: `http://${environment}:3000`,
+    baseUrl: `http://${HOST_FQDN}:3000`,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
